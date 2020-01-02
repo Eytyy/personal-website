c078659d322bc5764a248b4a2aa465e351f348ef
Eytyy <e.tayyem@gmail.com>
2020-01-02 20:13:11 +0200

    debugging environment vars

--- server/contentfulClient.js
+++ server/contentfulClient.js
@@ -1,7 +1,6 @@
 const { createClient } = require('contentful')
 const { accessToken, space } = require('./config')
-console.log(accessToken)
-console.log(process.env.ACCESS_TOKEN)
+
 module.exports = createClient({
   accessToken,
   space
